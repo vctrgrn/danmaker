5c5697f5b514d8433b98bbc53d512df7a81c5286
Aurélien Carteret <crtaurelien@gmail.com>
2018-03-15 23:01:20 +0100

    Ajout de bloc de contenu et redimensionnement du carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,36 +1,37 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <title>Danmaker</title>
-        <link rel="stylesheet" type="text/css" href="{{asset('/css/style.css')}}">
-        <link rel="shortcut icon" href="{{asset('/images/instagram.png')}}">
+    <title>Danmaker</title>
+    <link rel="shortcut icon" href="{{asset('/images/instagram.png')}}">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{asset('/css/style.css')}}">
 
-        <!-- Styles -->
-        <style>
+    <!-- Styles -->
+    <style>
 
-        </style>
-    </head>
-    <body>
+    </style>
+</head>
+<body>
 
-    <nav class="navbar navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">
-            <img src="{{asset('/images/instagram.png')}}" width="30" height="30" alt="">
-        </a>
-        <div class="form-inline">
-            <span class="userIcon"><img src="{{asset('/images/user.png')}}"></span>
-            <span class="panierIcon"><img src="{{asset('/images/cart-of-ecommerce.png')}}"></span>
-            <span class="searchIcon"><img src="{{asset('/images/magnifier.png')}}"></span>
-        </div>
-    </nav>
-
+<nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+        <img src="{{asset('/images/instagram.png')}}" width="30" height="30" alt="">
+    </a>
+    <div class="form-inline">
+        <span class="userIcon"><img src="{{asset('/images/user.png')}}"></span>
+        <span class="panierIcon"><img src="{{asset('/images/cart-of-ecommerce.png')}}"></span>
+        <span class="searchIcon"><img src="{{asset('/images/magnifier.png')}}"></span>
+    </div>
+</nav>
+<!-- CAROUSEL -->
+<div>
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
@@ -55,38 +56,133 @@
             <span class="sr-only">Next</span>
         </a>
     </div>
+</div>
 
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+<div class="row-gutter"> <!-- Permet de marger entre les conteneurs -->
+</div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    <img src="{{asset('https://image.noelshack.com/fichiers/2018/11/2/1520976609-gg.jpg')}}" width="150px" height="150">
-                        Frites au Caribou
-                    <img src="{{asset('https://d2homsd77vx6d2.cloudfront.net/cache/7/9/0ca70fffe05fcd99f8768e6cb14dd2.jpg')}}" width="150px" height="150">
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Dis donc</a>
-                    <a href="https://laracasts.com">y a du boulot</a>
-                    <a href="https://laravel-news.com">sur ce site.</a>
-                    <a href="https://forge.laravel.com">Faudrait penser à</a>
-                    <a href="https://github.com/laravel/laravel">s'y mettre!</a>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-3">
+            <div class="card">
+                <a href="#">
+                    <img class="card-img-top resized" src="http://danmaker.fr/img/p/8/7/8/878-home_default.jpg"
+                         style="display: block;" data-holder-rendered="true">
+                </a>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the
+                        card's content.</p>
                 </div>
             </div>
         </div>
+        <div class="col-3">
+            <div class="card">
+                <a href="#">
+                    <img class="card-img-top resized" src="http://danmaker.fr/img/p/8/7/8/878-home_default.jpg"
+                         style="display: block;" data-holder-rendered="true">
+                </a>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <a href="#">
+                    <img class="card-img-top resized" src="http://danmaker.fr/img/p/8/7/8/878-home_default.jpg"
+                         style="display: block;" data-holder-rendered="true">
+                </a>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <a href="#">
+                    <img class="card-img-top resized" src="http://danmaker.fr/img/p/8/7/8/878-home_default.jpg"
+                         style="display: block;" data-holder-rendered="true">
+                </a>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row-gutter"></div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <a href="#">
+                    <img class="card-img-top resized" src="http://danmaker.fr/img/p/8/7/8/878-home_default.jpg"
+                         style="display: block;" data-holder-rendered="true">
+                </a>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <a href="#">
+                    <img class="card-img-top resized" src="http://danmaker.fr/img/p/8/7/8/878-home_default.jpg"
+                         style="display: block;" data-holder-rendered="true">
+                </a>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <a href="#">
+                    <img class="card-img-top resized" src="http://danmaker.fr/img/p/8/7/8/878-home_default.jpg"
+                         style="display: block;" data-holder-rendered="true">
+                </a>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <a href="#">
+                    <img class="card-img-top resized" src="http://danmaker.fr/img/p/8/7/8/878-home_default.jpg"
+                         style="display: block;" data-holder-rendered="true">
+                </a>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row-gutter">
+</div>
 
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    </body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+</body>
 </html><!doctype html>
