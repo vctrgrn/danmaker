1f784bb9ac4018ad51519110dd565f8ff0a8a961
Aurélien Carteret <crtaurelien@gmail.com>
2018-03-15 19:41:33 +0100

    Update du slider, flèches previous et next de nouveaux fonctionnelles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,7 +31,7 @@
         </div>
     </nav>
 
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img class="d-block w-100" src="{{asset('/images/image1.jpg')}}" alt="First slide">
@@ -46,11 +46,11 @@
                 <img class="d-block w-100" src="{{asset('/images/image4.jpg')}}" alt="First slide">
             </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="false"></span>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
