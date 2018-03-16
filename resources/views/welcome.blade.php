5b57ad0a64b2018c01d06044a6e4bf5a541032da
Aurélien Carteret <crtaurelien@gmail.com>
2018-03-16 18:55:59 +0100

    commit inutile, à ne pas prendre en compte

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,8 +95,7 @@
         @endfor
     </div>
 </div>
-<div class="row-gutter">
-</div>
+
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
